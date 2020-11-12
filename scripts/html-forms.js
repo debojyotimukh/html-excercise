680760ec9615d33fff829fa4cd2bc7ca4be1185e
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-13 01:28:29 +0530

    implement password regex validation rules

--- scripts/html-forms.js
+++ scripts/html-forms.js
@@ -23,11 +23,24 @@ email.addEventListener('input', function () {
     else emailError.textContent = " Email is incorrect";
 });
 
-const phone=document.querySelector('#tel');
-const phoneError=document.querySelector('.phone-class');
-phone.addEventListener('input',function () {
-   let phoneRegex=RegExp('^([0-9]{2})+([ ])+([0-9]{10})$');
-   if(phoneRegex.test(phone.value))
-        phoneError.textContent="";
-    else phoneError.textContent=" Phone is incorrect";
+const phone = document.querySelector('#tel');
+const phoneError = document.querySelector('.phone-class');
+phone.addEventListener('input', function () {
+    let phoneRegex = RegExp('^([0-9]{2})+([ ])+([0-9]{10})$');
+    if (phoneRegex.test(phone.value))
+        phoneError.textContent = "";
+    else phoneError.textContent = " Phone is incorrect";
+});
+
+const password = document.querySelector('#pwd');
+const passwordError = document.querySelector('.pwd-class');
+password.addEventListener('input', function () {
+    let pwdRegex1 = RegExp('[A-Za-z]');
+    let pwdRegex2 = RegExp('.*[A-Z]');
+    let pwdRegex3 = RegExp('.*[0-9]');
+    let pwdRegex4 = RegExp('.{8,}');
+    let splCharCount = (password.value.match(new RegExp("[\\_\\?\\#\\@\\#]", "g")) || []).length;
+    if (pwdRegex1.test(password.value) && pwdRegex2.test(password.value) && pwdRegex3.test(password.value) && pwdRegex4.test(password.value) && splCharCount == 1)
+        passwordError.textContent = "";
+    else passwordError.textContent = " Password is illegal";
 });
